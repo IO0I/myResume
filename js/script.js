79d24a0eeda6ff1b1a7b94b372f0234811d64f40
Abdullah-Mohamed94 <74384366+Abdullah-Mohamed94@users.noreply.github.com>
2023-08-28 17:58:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*typing animation*/
+var typed =new Typed(".typing",{
+    strings:["DevOps1 Engineer1","DevOps2 Engineer2","DevOps3 Engineer3"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+/****
+ * 
+ * 
+ * لو ضفت ""  فاضيين بعد كل جمله هتتغير طريقة العرض*
+ * 
+ * مثال
+ * 
+ *     strings:["","DevOps1 Engineer1","","DevOps2 Engineer2","","DevOps3 Engineer3"],
+ * 
+ * ** */
